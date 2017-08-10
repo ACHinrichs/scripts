46eccbf2c31c4d68c78a17da88bc0527cca39490
ACHinrichs <adrian-corin.hinrichs@gmx.de>
2017-08-10 22:48:01 +0200

    Readd script
    Signed-off-by: ACHinrichs <adrian-corin.hinrichs@gmx.de>

---
+++ spotify_info.py
@@ -0,0 +1,27 @@
+'''
+Script to output the Song & artist currently playd on spotify. 
+
+Intended for usage in my Polybar
+Should work with both python3 and python2, but I develop it under python3
+
+WORKS ON LINUX ONLY
+
+Partially stolen from https://stackoverflow.com/questions/33883360/get-spotify-currently-playing-track
+'''
+
+import dbus
+session_bus = dbus.SessionBus()
+spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify",
+                                     "/org/mpris/MediaPlayer2")
+spotify_properties = dbus.Interface(spotify_bus,
+                                    "org.freedesktop.DBus.Properties")
+metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
+
+
+artist = ""
+for key in metadata['xesam:artist']:
+    if not artist=="":
+        artist=artist+", "
+    artist=artist + key
+    
+print(metadata['xesam:title']+" - "+artist)
