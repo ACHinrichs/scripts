83f8abd79bbf3c33e0b856bfff3491971b426c73
ACHinrichs <adrian-corin.hinrichs@gmx.de>
2017-09-17 22:23:08 +0200

    Start with TUI Dialog

--- abrechnung/abrechnung.py
+++ abrechnung/abrechnung.py
@@ -2,17 +2,80 @@
 # -*- coding: utf-8 -*-
 from subprocess import call
 import datetime
-
+import math
 import config_private as config
+import curses
+from calendar import monthrange
+
+
+def TUICalendar:
+
+  def printDays(win, year, month, active, tly=3, tlx=1, normalCP=1):
+    win.bkgd(curses.color_pair(normalCP))
+    
+    startCW=datetime.date(year,month,1).isocalendar()[1]
+    
+    win.addstr(tly,tlx+3,"Mo Di Mi Do Fr Sa So")
+    for day in range(1,monthrange(year, month)[1]):
+      ic=datetime.date(year,month,day).isocalendar()
+      cw=ic[1]
+      wd=ic[2]
+      win.addstr(tly+2+(cw-startCW) * 2,
+                 tlx+wd*3,
+                 "%2d"%day)
+      
+      win.move(tly,tly)
+      win.refresh()
 
 print("Erstelle die Abrechnung")
 
 monate = ["","Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober","November","Dezember"]
 tage = ["","Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"]
-month = int(float(raw_input("Welcher Monat?(Zahl; Standard: %d) " % datetime.datetime.now().month))) or datetime.datetime.now().month
+
+month = raw_input("Welcher Monat?(Zahl; Standard: %d) " % datetime.datetime.now().month) or str(datetime.datetime.now().month)
+month= int(month)
 year = raw_input("Welches Jahr?(Zahl; Standard: %d) " % datetime.datetime.now().year) or datetime.datetime.now().year
+
+
 body = "Hallo Herr "+config.name+",\n\nhier meine Abrechnung für "+monate[month]+" %d:\n\n"%(year)
-userInput = raw_input("Tag: ")
+
+
+stdscr = curses.initscr()
+curses.noecho()
+curses.cbreak()
+stdscr.keypad(1)
+curses.start_color()
+curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
+curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
+curses.init_pair(4, curses.COLOR_BLACK, curses.COLOR_BLUE)
+
+stdscr.bkgd(curses.color_pair(1))
+stdscr.refresh()
+
+curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
+win = curses.newwin(15, 25, 1, 1,)
+win.bkgd(curses.color_pair(1))
+win.box()
+win.addstr(1, 1, str(monate[month])+" "+str(year))
+
+win.refresh()
+
+printDays(win,year,month,-1)
+
+day=1
+dayInfo = datetime.date(year,month,day).isocalendar()
+
+# Warten auf Tastendruck
+c = stdscr.getch()
+
+# Ende
+curses.nocbreak()
+stdscr.keypad(0)
+curses.echo()
+curses.endwin()
+
 moneyTotal = 0
 lastKW = 0
 while not(userInput == ""):
@@ -45,5 +108,5 @@ while not(userInput == ""):
 body = body + "------------------------------------------------\n"
 body = body + " = %1.2f\n" % moneyTotal
 body = body+"\nMit freundlichen Grüßen,\nAdrian Hinrichs"
-print body
+print(body)
 call(["thunderbird","-compose","""to='"""+config.email+"""',subject='Abrechnung ',body='""" + body + "'"""])
