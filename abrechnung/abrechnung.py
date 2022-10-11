5d12f32c8946124d4de217d0bd9c8e54fe286e71
Adrian Hinrichs <adrian.hinrichs@rwth-aachen.de>
2022-10-11 21:22:40 +0200

    Change to Weekly format

--- abrechnung/abrechnung.py
+++ abrechnung/abrechnung.py
@@ -36,7 +36,7 @@ parser.add_argument('-y','--year',
                     required=False,
                     default=now.year)
 
-BEGRUESSUNG = f"Hallo {config.name},\n"
+BEGRUESSUNG = f"Moin moin,\n"
 SCHLUSSFORMEL = "Alles Gute,\nAdrian"
 GEHALT = 12
 class TrainingsSlot:
@@ -68,6 +68,12 @@ class Training:
     self.month=month
     self.year=year
 
+  @property
+  def woche(self):
+    ic=datetime.date(self.year,self.month,self.day).isocalendar()
+    cw=ic[1]
+    return cw
+    
   def lohn(self):
     return self.slot.duration * GEHALT
 
@@ -86,7 +92,8 @@ wochen_trainings = [{TrainingsSlot("17:00 - 18:30 Uhr", 1.5, True), # Montag
                      TrainingsSlot("17:45 - 19:15 Uhr", 1.5, True ),
                      #TrainingsSlot("18:00 - 19:00 Uhr (Online)",1)
                      },
-                    {TrainingsSlot("17:00 - 18:15 Uhr", 1.25)}, # Donnerstag
+                    {TrainingsSlot("17:00 - 18:15 Uhr", 1.25), # Donnerstag
+                     TrainingsSlot("18:30 - 20:00 Uhr", 1.5)}, # Donnerstag
                     {TrainingsSlot("17:00 - 18:30 Uhr", 1.5),
                      TrainingsSlot("18:30 - 19:30 Uhr", 1, True), # 
                      #TrainingsSlot("19:00 - 20:00 Uhr", 1)
@@ -122,7 +129,7 @@ class AbrechnerApp(npyscreen.NPSApp):
                values = trainings, scroll_exit=True)
     F.edit()
     self.result=sorted([ms2.values[i] for i in ms2.value], key=lambda t: t.day)
-      
+    print(self.result)
 
 if __name__ == "__main__":
   args=parser.parse_args()
@@ -134,13 +141,30 @@ if __name__ == "__main__":
 
   print("====== ABRECHNUNG ======")
   lohn_ges = 0
+  stunden_ges = 0
   body = BEGRUESSUNG
   body = body + "\nhier meine Abrechnung für "+monate[args.month]+" %d:\n\n"%(args.year)
+  body += "Woche\tgel. Stunden à 12,00 €/h\t€\n"
+  first_week = datetime.date(App.year,App.month,1).isocalendar()[1]
+  last_week = datetime.date(App.year,App.month,monthrange(App.year, App.month)[1]).isocalendar()[1]
+  print(monthrange(App.year, App.month)[1])
+  weeks = {}
+  for week in range(first_week, last_week+1):
+    weeks[week] = 0
+  
   for training in App.result:
-    body = body + str(training) + "\n"
-    lohn_ges=lohn_ges + (training.lohn())
+    if training.woche in weeks:
+      weeks[training.woche] += training.slot.duration
+    else:
+      weeks[training.woche] = training.slot.duration
+    print(str(training))
+  for (kw,stunden) in weeks.items():
+    geld_kw = stunden * GEHALT
+    body += f"{kw}\t{stunden}\t\t\t\t{geld_kw:.2f}€\n"
+    lohn_ges += geld_kw
+    stunden_ges += stunden
   body = body + "--------------------------------------------------------\n"
-  body = body + f"\t\t\t\t\t\t={(lohn_ges):.2f}€\n"
+  body = body + f"Summe\t{stunden_ges}\t\t\t\t{(lohn_ges):.2f}€\n"
   body = body + SCHLUSSFORMEL
   print(body)
-  call(["thunderbird","-compose",f"""format=2,to={config.email},subject='Abrechnung Training """+monate[args.month]+"""',body='""" + body + "'"""])
+  #call(["thunderbird","-compose",f"""format=2,to={config.email},subject='Abrechnung Training """+monate[args.month]+"""',body='""" + body + "'"""])
